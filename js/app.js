76cf3a1b803a0a2945464372c02589a86f88e3d0
Franklin Zuniga <Franklin@slyle-mac15.local>
2016-03-28 01:22:18 -0700

    add images, tried getting a winner, but no luck

--- js/app.js
+++ js/app.js
@@ -10,33 +10,71 @@ $(document).on('ready', function() {
 
 
 function firstPlayerMove() {
-  $(window).on('keyup', function(event1) {
+  $(window).on('keyup', function(event) {
        var player1 = $("#player1");
        var right = 65;
 
 
-      if (event1.keyCode == right) {
-           player1.animate({left:"+=20"},5);
+      if (event.keyCode === right) {
+           player1.animate({left:"+=20"}, 5);
        }
+
   });
 
 }
 function secondPlayerMove() {
-  $(window).on('keyup', function(event1) {
+  $(window).on('keyup', function(event) {
        var player2 = $("#player2");
        var right = 76;
 
 
-      if (event1.keyCode == right) {
-           player2.animate({left:"+=20"},5);
+      if (event.keyCode === right) {
+           player2.animate({left:"+=20"}, 5);
+
        }
+
   });
 }
 
 function getWinner() {
 
+  if(player1.css('left') === '920px') {
+    alert("player one wins!");
+  } else if (player2.css('left') === '920px') {
+    alert("player two wins!");
+  }
 
+  getWinner();
 }
+
+$('#reset').on("click" , function resetGame(e){
+    player1.animate ({
+      'left': '0px'
+    });
+    player2.animate({
+      'left': '0px'
+    });
+
+    resetGame();
+  });
+
+// $('#reset').on("click" , function resetGame(e){
+// playerOne.animate ({
+//   'left': '0px'
+// });
+// playerTwo.animate({
+//   'left': '0px'
+// } ,0 );
+
+// function getWinner() {
+//   if (player1.width >= "=1000px") {
+//     alert('1 wins');
+//   } else if (player2.width >= "=1000px") {
+//     alert('2 wins');
+//   } else {
+//
+//   }
+// }
 
 
 firstPlayerMove();
